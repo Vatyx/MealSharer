50563bf6905f6307c2542fdcfd5e0296b0a44982
Himank Yadav <him229@gmail.com>
2015-11-16 01:57:28 -0600

    some work left

--- MessagingTutorialSkeleton/src/main/java/com/sinch/messagingtutorialskeleton/LoginActivity.java
+++ MessagingTutorialSkeleton/src/main/java/com/sinch/messagingtutorialskeleton/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.sinch.messagingtutorialskeleton;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -45,8 +46,10 @@ public class LoginActivity extends Activity {
 
         ParseUser currentUser = ParseUser.getCurrentUser();
         if (currentUser != null) {
-            //start next activity
-            //start sinch service
+            final Intent intent = new Intent(getApplicationContext(), ListUsersActivity.class);
+            final Intent serviceIntent = new Intent(getApplicationContext(), MessageService.class);
+            startActivity(intent);
+            startService(serviceIntent);
         }
 
         loginButton = (Button) findViewById(R.id.loginButton);
@@ -62,8 +65,10 @@ public class LoginActivity extends Activity {
                 ParseUser.logInInBackground(username, password, new LogInCallback() {
                     public void done(ParseUser user, com.parse.ParseException e) {
                         if (user != null) {
-                            //start next activity
-                            //start sinch service
+                            final Intent intent = new Intent(getApplicationContext(), ListUsersActivity.class);
+                            final Intent serviceIntent = new Intent(getApplicationContext(), MessageService.class);
+                            startActivity(intent);
+                            startService(serviceIntent);
                         } else {
                             Toast.makeText(getApplicationContext(),
                                     "There was an error logging in.",
